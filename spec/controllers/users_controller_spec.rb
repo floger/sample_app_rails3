26ac77239a5dd942953692de772257037c21aea5
floger <wangshuangjun0210@gmail.com>
2012-02-06 10:25:20 +0800

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,6 +6,54 @@ describe UsersController do
   
   render_views
 
+  describe "POST 'create'" do
+
+    describe "failure" do
+      before(:each) do
+        @attr ={:name => "",:email=> "",:password =>"",:password_confirmation => ""}
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create,:user => @attr
+        end.should_not change(User,:count)
+      end
+
+      it "should have a right title" do
+        post :create,:user => @attr
+        response.should have_selector("title",:content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template("new") 
+      end
+    end
+
+    describe  "successful" do
+      before(:each) do
+        @attr = {:name => "New User",:email => "user@foo.com",:password =>"foobar",:password_confirmation=>"foobar"}
+      end
+      
+      it "should create a user " do
+        lambda do
+          post :create,:user => @attr 
+        end.should change(User,:count).by(1)        
+      end
+
+      it "should redirect to the user show page" do
+        post :create,:user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create,:user => @attr
+        flash[:success].should =~ /welcome come to simple app/i
+      end
+    end
+    
+  end
+
   describe "GET 'show'" do
     before(:each) do
       @user = Factory(:user)
@@ -41,12 +89,12 @@ describe UsersController do
   describe "GET 'new'" do
 
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have a right title" do
-      get 'new'
+      get :new
       response.should have_selector("title",:content => 'Sign up')
     end
   end
